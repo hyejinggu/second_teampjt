420cf6dc8168e2ec2467f57890a48105ef1e6c91
User -2023BEGZK\Administrator <s__h8609@naver.com>
2023-08-07 17:50:49 +0900

    header 연결

--- react_pjt/src/App.js
+++ react_pjt/src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from "./components/Main"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
