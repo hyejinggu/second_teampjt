7a41038a6d2a38a6e1692ea43a0198033153b37c
hyejinggu <118355536+hyejinggu@users.noreply.github.com>
2023-08-08 23:17:16 +0900

    리액트 - 이미지 슬라이드, 알림 배너 구현

--- react_pjt/src/components/common/Main.jsx
+++ react_pjt/src/components/common/Main.jsx
@@ -1,9 +1,12 @@
 import Slide from "../main/Slide";
+import NoticeBanner from "../main/NoticeBanner";
+import "../../css/main.css";
 
 const Main = () => {
   return (
     <div>
       <Slide />
+      <NoticeBanner />
     </div>
   );
 };
