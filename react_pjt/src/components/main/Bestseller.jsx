580d0b702518ebd2563f6b10c8fb8b142ebcb4aa
LAPTOP-R21IOJ0I\User <ehdrhq@gmail.com>
2023-08-09 01:55:09 +0900

    베스트셀러 중복코드 map으로 수정

--- react_pjt/src/components/main/Bestseller.jsx
+++ react_pjt/src/components/main/Bestseller.jsx
@@ -1,4 +1,29 @@
-import '../../common/css/Bestseller.css'
+
+import '../../css/Bestseller.css'
+
+const products = [
+    {
+        name: "(간식) 진짜 연어",
+        saleInfo: "30%",
+        normalPr: "39,900원",
+        presentPr: "27,900원",
+    }, {
+        name: "(수입) 멍멍 사료",
+        saleInfo: "20%",
+        normalPr: "39,900원",
+        presentPr: "31,900원",
+    }, {
+        name: "하루 유산균",
+        saleInfo: "10%",
+        normalPr: "40,000원",
+        presentPr: "36,000원",
+    }, {
+        name: "아침애 사료 (다이어트)",
+        saleInfo: "30%",
+        normalPr: "29,900원",
+        presentPr: "20,900원",
+    },
+];
 
 const Bestseller = () => {
 
@@ -9,7 +34,7 @@ const Bestseller = () => {
                 <a href="#">
                     <h2>간식, 사료</h2>
                 </a>
-                <a href="./pjt/main/html/main_best_toy.html">
+                <a href="">
                     <h2>장난감</h2>
                 </a>
                 <a href="">
@@ -19,57 +44,21 @@ const Bestseller = () => {
                     <h2>산책, 케어</h2>
                 </a>
             </div>
+
             <ul className="bestseller">
-                <li>
-                    <div className="item_info_wrap">
-                        <a href="#">
-                        {/* bestseller2.png */}
-                            <img src={"../../public/images/logo.png"} alt="상품 1" />
-                                <h3>(간식) 진짜 연어</h3>
-                                <span className="sale_info">30%</span>
-                                <span className="normal_pr">39,900원</span>
-                                <span className="present_pr">27,900원</span>
-                        </a>
-                    </div>
-                </li>
-                <li>
-                    <div className="item_info_wrap">
-                        <a href="#">
-                            {/* <img src="./pjt/main/image/bestseller3.jpeg" alt="상품 2"> */}
-                            <img src={"../../public/images/logo.png"} alt="상품 1" />
-                                <h3>(수입) 멍멍 사료</h3>
-                                <span className="sale_info">20%</span>
-                                <span className="normal_pr">39,900원</span>
-                                <span className="present_pr">31,900원</span>
-                        </a>
-                    </div>
-                </li>
-                <li>
-                    <div className="item_info_wrap">
-                        <a href="#">
-                            {/* <img src="./pjt/main/image/bestseller4.png" alt="상품 2"> */}
-                            <img src={"../../public/images/logo.png"} alt="상품 1" />
-                                <h3>하루 유산균</h3>
-                                <span className="sale_info">10%</span>
-                                <span className="normal_pr">40,000원</span>
-                                <span className="present_pr">36,000원</span>
-                        </a>
-                    </div>
-                </li>
-                <li>
-                    <div className="item_info_wrap">
-                        <a href="#">
-                            {/* <img src="./pjt/main/image/bestseller5.png" alt="상품 2"> */}
-                            <img src={"../../public/images/logo.png"} alt="상품 1" />
-                                <h3>아침애 사료 (다이어트)</h3>
-                                {/* <span className="sale_info">30%</span>
-                                <span className="normal_pr">30%</span> */}
-                                <span className="sale_info">30%</span>
-                                <span className="normal_pr">29,900원</span>
-                                <span className="present_pr">20,900원</span>
-                        </a>
-                    </div>
-                </li>
+                {products.map((product, index) => (
+                    <li key={index}>
+                        <div className="item_info_wrap">
+                            <a href="#">
+                                <img src={"../../public/images/logo.png"} alt={`상품 ${index + 1}`} />
+                                <h3>{product.name}</h3>
+                                <span className="sale_info">{product.saleInfo}</span>
+                                <span className="normal_pr">{product.normalPr}</span>
+                                <span className="present_pr">{product.presentPr}</span>
+                            </a>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     );
