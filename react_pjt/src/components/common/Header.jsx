a12b9805c53abd8e1b14fe72c2be1b9ac7d413f1
namuri23 <>
2023-08-09 17:05:50 +0900

    헤더 이미지링크 수정

--- react_pjt/src/components/common/Header.jsx
+++ react_pjt/src/components/common/Header.jsx
@@ -21,24 +21,24 @@ const Header = () => {
 
                 <ul>
                     <li>
-                        <a href="./pjt/login/html/login.html">
+                        <a href="./react_pjt/login/html/login.html">
                             <img src={"/images/header/login_img.png"} alt="" /> 로그인
                         </a>
                     </li>
                     <li>
-                        <a href="./pjt/join/html/profile.html">
+                        <a href="./react_pjt/join/html/profile.html">
                             <img src={"/images/header/join_img.png"} alt="" />
                             회원가입
                         </a>
                     </li>
                     <li>
-                        <a href="./pjt/cart/html/cart.html">
+                        <a href="./react_pjt/cart/html/cart.html">
                             <img src={"/images/header/cart_img.png"} alt="" />
                             장바구니
                         </a>
                     </li>
                     <li>
-                        <a href="./pjt/board/html/board.html">
+                        <a href="./react_pjt/board/html/board.html">
                             <img src={"/images/header/customer_service_img.png"} alt="" />
                             고객센터
                         </a>
@@ -47,20 +47,34 @@ const Header = () => {
             </div>
             <ul class="gnb_list">
                 <li>
-                    <a href="#"><img src="../../../common_page/image/header/food_img.png" alt="" />간식&#183;사료</a>
+                    <a href="#">
+                        <img src={"/images/header/food_img.png"} alt="" />
+                        간식&#183;사료
+                    </a>
                 </li>
                 <li>
-                    <a href="../../subpage/html/subpage1_toy.html"><img
-                        src="../../../common_page/image/header/toy_img.png" alt="" />장난감</a>
+                    <a href="../../subpage/html/subpage1_toy.html">
+                        <img src={"/images/header/toy_img.png"} alt="" />
+                        장난감
+                    </a>
                 </li>
                 <li>
-                    <a href="#"><img src="../../../common_page/image/header/living_img.png" alt="" />리빙&#183;패션</a>
+                    <a href="#">
+                        <img src={"/images/header/living_img.png"} alt="" />
+                        리빙&#183;패션
+                    </a>
                 </li>
                 <li>
-                    <a href="#"><img src="../../../common_page/image/header/stroll_img.png" alt="" />산책&#183;케어</a>
+                    <a href="#">
+                        <img src={"/images/header/stroll_img.png"} alt="" />
+                        산책&#183;케어
+                    </a>
                 </li>
                 <li>
-                    <a href="../../subpage/html/community_lounge.html"><img src="../../../common_page/image/header/community_img.png" alt="" />커뮤니티</a>
+                    <a href="../../subpage/html/community_lounge.html">
+                        <img src={"/images/header/community_img.png"} alt="" />
+                        커뮤니티
+                    </a>
                 </li>
             </ul>
         </header>
