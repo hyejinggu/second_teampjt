6a4e6c4acdd9e7330045730bd0761d7b4c18d495
User -2023CHGYS\Administrator <bmhsr5@naver.com>
2023-08-10 16:54:02 +0900

    커뮤니티 페이지 시작

---
+++ react_pjt/src/components/community/Lounge.jsx
@@ -0,0 +1,234 @@
+import "../../css/subpage/community_lounge";
+import PageNation from "../item/PageNation";
+
+export default function Lounge() {
+  return (
+    <div id="wrap">
+      <div class="container">
+        <div class="title">
+          <strong>
+            <a href="#">라운지</a>
+            <a href="">정보 공유&amp;리뷰</a>
+            <a href="">QnA</a>
+            <a href="">이벤트</a>
+          </strong>
+        </div>
+
+        <div class="post sort">
+          <ul>
+            <li>인기글</li>
+            <li>공지사항</li>
+            <li>최신순</li>
+            <li>오래된순</li>
+          </ul>
+          <p>총 50개의 글</p>
+        </div>
+
+        <div class="content_wrap">
+          <table>
+            <thead>
+              <tr>
+                <th>번호</th>
+                <th>이미지</th>
+                <th>제목 및 내용</th>
+                <th>날짜</th>
+                <th>추천수</th>
+                <th>조회수</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/405-20230618_200323.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>집 앞 공원으로 산책 다녀왔어요.</h4>
+                    <p>
+                      발톱이 살짝들려서 발에 붕대하고 있는데 집에만 있음 넘
+                      답답해할까봐 살짝 나와서 산책해줬어요❣️ 잠깐 나와도
+                      이렇게나 좋은지.. 발 다나으면 산책 원없이 해줄꼬에요😆
+                    </p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/555-image_picker8501982540016886358.jpg"
+                      alt="#"
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>병원진료도 잘받아요🤣</h4>
+                    <p>
+                      병원에서 얌전하게 진료도 잘받는다고 칭찬받았어요😆 근데
+                      내가하면 승질 엄청내던데..
+                    </p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/758-image_picker3081517976164242868.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>가슴으로 낳아 돈으로 키운다</h4>
+                    <p>🐶🐶너무맞는말이쟎아요🤍 건강만해다오. 댕댕이들아</p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/828-image_picker_8C352A29-9E68-485B-93A4-FEB70D5A6C2D-5498-000007EF9298DA7B.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>몇 살 같아 보이나요?! ㅎㅎ</h4>
+                    <p>
+                      2011년 1월생이에요 무려 12살 최강 동안...
+                      이지않나요?!ㅎㅎㅎㅎ
+                    </p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/273-KakaoTalk_20230617_061331272_01.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="">
+                    <h4>이 생명체는 누구일까요!</h4>
+                    <p>
+                      뭣좀하려고 컴퓨터앞에 앉아있음 꼭 밑에서 등돌리고 저러고
+                      누워있는데 뒷모습 털쪘다요❤️ 너구린가 댕댕인가 착각들정도
+                    </p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/535-20230528_165840.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>대형견 산책시 뛰고 또 뛰고~~</h4>
+                    <p>
+                      풍산개 수컷입니다. 아침 산책시간만 되면 흥분지수가 업
+                      입니다. 견사 문 열자마자(지가 열려고 난리 치죠)
+                      후다닥..여긴 가평이라 그나마 도심지가 아니지만 앞서서 뛰고
+                      달리고, 산으로 산속으로 더 들어가려고 감당이 되지않네요.
+                      30개월 된 친구인데..평소엔 먹는거 좋아라하는거말고
+                      순둥이인데..짖지도 않고, 소형견 지나가면 짖긴
+                      하지만~~산책시 먹는것으로 유도해보려 하는데먹는거
+                      좋아라하는 얘가 밖에선 쳐다보지도 않아요. 온갖 냄새는 지가
+                      다 맡아야 하는겐지.. 겨울이 벌써 걱정이예요. 무서워서 못
+                      나갈 듯 싶어서요.
+                    </p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/943-image_picker4043895192963572728.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>간식ㅎㅎ</h4>
+                    <p>간식먹고싶어서낼름ㅎ</p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>
+                  <a href="#">
+                    <img
+                      src="https://bff-images.bemypet.kr/media/medias/all/749-image_picker_E73C8D6C-5E69-4E01-A994-AE950E6A4CB8-81072-00001075AD518031.jpg"
+                      alt=""
+                    />
+                  </a>
+                </td>
+                <td>
+                  <a href="#">
+                    <h4>강아지 썬캡 샀어용</h4>
+                    <p>
+                      검진 했더니 예방차 자외선 차단 해주라구 해서 썬캡 샀는데
+                      넘 귀여워용~~^^
+                    </p>
+                  </a>
+                </td>
+                <td>2023.06.20</td>
+                <td>12</td>
+                <td>50</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <PageNation />
+    </div>
+  );
+}
